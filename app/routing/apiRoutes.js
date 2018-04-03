f026ffd9daa371dc13c185a2d2f108528c40258f
aolaleye <30451506+aolaleye@users.noreply.github.com>
2018-04-03 19:54:53 -0400

    added more friends to friendsArray

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,21 +10,48 @@ module.exports = function(app) {
 
     app.post("/api/friends", function(req, res) {
 
-        if (userData.length < 5) {
-            userData.push(req.body);
-            res.json(true);
-        } else {
-            userData.push(req.body);
-            res.json(false);
+        userData.push(req.body);
+        res.json(true);
+
+        var userInput = req.body;
+        var userRes = userInput.scores;
+
+        var match = "";
+        var matchImg = "";
+        var friendDiff = 100;
+
+        for (i = 0; i < friends.length; i++) {
+            var diff = 0;
+            for (j = 0; j < userRes.length; j++) {
+                diff += Math.abs(friends[i].scores[j] - userRes[j]);
+                console.log(diff);
+            }
+            console.log("diff = " + diff);
+
+            if (diff < friendDiff) {
+                friendDiff = diff;
+                match = friends[i].name;
+                matchImg = friends[i].pic;
+                console.log("match diff = " + diff);
+                console.log("name = " + match);
+                console.log("image = " + matchImg);
+            }
         }
+
+        friends.push(userInput);
+
+        res.json({
+            matchName: match,
+            matchImg: matchImg
+        });
 
     });
 
-  app.post("/api/clear", function() {
+    app.post("/api/clear", function() {
 
-    userData = [];
+        userData = [];
 
-    console.log(userData);
+        console.log(userData);
 
-  });
+    });
 };

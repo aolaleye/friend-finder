ab3cb02c91a7c99c8c2917f3993804b01ad05b33
aolaleye <30451506+aolaleye@users.noreply.github.com>
2018-04-02 23:14:41 -0400

    added content to js and html files

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,30 @@
+var userData = require("../data/userData");
+
+module.exports = function(app) {
+  
+    app.get("/api/friends", function(req, res) {
+
+        res.json(userData);
+
+    });
+
+    app.post("/api/friends", function(req, res) {
+
+        if (userData.length < 5) {
+            userData.push(req.body);
+            res.json(true);
+        } else {
+            userData.push(req.body);
+            res.json(false);
+        }
+
+    });
+
+  app.post("/api/clear", function() {
+
+    userData = [];
+
+    console.log(userData);
+
+  });
+};

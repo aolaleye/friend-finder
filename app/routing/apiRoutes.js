6830eef84e709811f7fa66e0eba1d26027a40bc8
aolaleye <30451506+aolaleye@users.noreply.github.com>
2018-04-03 21:34:16 -0400

    updated console.log messages in the for loops in apiRoutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,22 +13,23 @@ module.exports = function(app) {
         var userSurvey = req.body;
         var userScores = userSurvey.scores;
 
+        var maxDifference = 50;
         var matchName;
         var matchPhoto;
-        var maxDifference = 50;
 
         for (i = 0; i < userData.length; i++) {
+
             var scoreDifference = 0;
+            
             for (j = 0; j < userScores.length; j++) {
                 scoreDifference += Math.abs(userData[i].scores[j] - userScores[j]);
             }
-            console.log("scoreDifference = " + scoreDifference);
 
             if (scoreDifference < maxDifference) {
                 maxDifference = scoreDifference;
                 matchName = userData[i].name;
                 matchPhoto = userData[i].photo;
-                console.log("Match Name = " + matchName + "\nScore Difference = " + scoreDifference);
+                console.log("\nMatch Name = " + matchName + "\nScore Difference = " + scoreDifference);
             }
         }
 
